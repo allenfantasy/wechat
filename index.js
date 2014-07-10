9dab7cc90c9a42eec4f04e96231caf60dd9abd05
Allen Wu <allenwu1018@gmail.com>
2014-07-10 08:38:30 +0800

    add oauth

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var WechatMessage = require('./lib/wechat-message.js')
     , wechatHelper = require('./lib/wechat-message-builder.js')
-    , wechatAuth = require('./lib/weixinauth.js');
+    , wechatAuth = require('./lib/weixinauth.js')
+    , wechatOAuth = require('./lib/weixinoauth.js');
 
 
 module.exports = {
     'WechatMessage': WechatMessage, //class of a wechat message
-    'wechatHelper': wechatHelper, //generates an express middleware that process wechat messages 
-    'wechatAuth': wechatAuth
+    'wechatHelper': wechatHelper, //generates an express middleware that process wechat messages
+    'wechatAuth': wechatAuth,
+    'wechatOAuth': wechatOAuth
     };
